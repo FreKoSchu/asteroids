46e60865f2bf087ffdea3ce26825ecfa89440da4
FreKoSchu <frederik.k.schubert@gmail.com>
2026-02-18 19:14:47 +0100

    added the player class that inherits from circleshape. also extended the draw method.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 import constants
 from logger import log_state
+from player import Player
 
 def main():
     
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
     dt = 0
     while True:
         log_state()
@@ -16,9 +18,14 @@ def main():
             
         dt=clock.tick(60)/1000
             
-            
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+    
+    
+
 
 
 if __name__ == "__main__":
