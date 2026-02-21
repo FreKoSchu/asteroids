c1e39a6fccaab7ebd6804444601eb42d82788c43
FreKoSchu <frederik.k.schubert@gmail.com>
2026-02-21 12:04:24 +0100

    created the classes asteroid and asteroidfield. integrated them in the main file and added new constants for the Asteroidfiled

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import pygame
 import constants
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
+    
+    asteroidfield = AsteroidField()
 
     
     
