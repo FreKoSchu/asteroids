458a229ae50ae465efe8da8cd208889f7541ea83
FreKoSchu <frederik.k.schubert@gmail.com>
2026-02-24 22:01:43 +0100

    added the split method in to the Asteroid class. when called it splits an asteroid and creates two smaller ones  if the radius is bigger than the min radius size. if not it kils the asteroid. Replaced the Asteroid.kill() with Asteroid.split()

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,8 @@
 from circleshape import CircleShape
 import pygame
 import constants
-
+from logger import log_event
+import random
 
 
 
@@ -15,3 +16,22 @@ class Asteroid(CircleShape):
 
     def update(self,dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+
+        self.kill()
+        if self.radius <= constants.ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        asteroid_one_vector = self.velocity.rotate(angle)
+        asteroid_two_vector = self.velocity.rotate((angle * (-1)))
+        new_radius = self.radius - constants.ASTEROID_MIN_RADIUS
+        asteroid_one = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid_one.velocity = asteroid_one_vector * 1.2
+        asteroid_two = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid_two.velocity = asteroid_two_vector * 1.2
+        
+
+
+        
